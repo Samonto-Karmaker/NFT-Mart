8f40c9cdf2dbd1ab6dacf6ec82475a660e8acc3b
Samonto Karmaker <samonto0726@gmail.com>
2025-05-08 17:16:23 +0600

    feat: add tests for listing NFTs in NftMart contract

--- backend-web3/test/NftMart.test.js
+++ backend-web3/test/NftMart.test.js
@@ -1,4 +1,5 @@
 const { ethers } = require("hardhat")
+const { expect, assert } = require("chai")
 const { developmentChains } = require("../helper-hardhat-config")
 
 !developmentChains.includes(network.name)
@@ -20,5 +21,55 @@ const { developmentChains } = require("../helper-hardhat-config")
               nft = await nftContract.deploy()
               await nft.mintNft()
               await nft.approve(nftMart.target, tokenId)
+          })
+
+          describe("listNFT", function () {
+              // not owner can't list
+              it("reverts if not owner", async () => {
+                  await expect(
+                      nftMart
+                          .connect(player)
+                          .listNFT(nft.target, tokenId, price),
+                  ).to.be.revertedWithCustomError(nftMart, "NotOwner")
+              })
+              // nft price can't be 0
+              it("reverts if price is 0", async () => {
+                  await expect(
+                      nftMart.listNFT(nft.target, tokenId, 0),
+                  ).to.be.revertedWithCustomError(
+                      nftMart,
+                      "PriceMustBeGreaterThanZero",
+                  )
+              })
+              // is event emitted
+              it("emits event when listed", async () => {
+                  await expect(nftMart.listNFT(nft.target, tokenId, price))
+                      .to.emit(nftMart, "NftListed")
+                      .withArgs(deployer.address, nft.target, tokenId, price)
+              })
+              // not approved can't list
+              it("reverts if not approved", async () => {
+                  await nft.approve(ethers.ZeroAddress, tokenId)
+                  await expect(
+                      nftMart.listNFT(nft.target, tokenId, price),
+                  ).to.be.revertedWithCustomError(nftMart, "NotApproved")
+              })
+              // is token properly listed
+              it("lists the token", async () => {
+                  await nftMart.listNFT(nft.target, tokenId, price)
+                  const listedNft = await nftMart.getListing(
+                      nft.target,
+                      tokenId,
+                  )
+                  assert(listedNft.price.toString() == price.toString())
+                  assert(listedNft.seller == deployer.address)
+              })
+              // can't list already listed token
+              it("reverts if already listed", async () => {
+                  await nftMart.listNFT(nft.target, tokenId, price)
+                  await expect(
+                      nftMart.listNFT(nft.target, tokenId, price),
+                  ).to.be.revertedWithCustomError(nftMart, "AlreadyListed")
+              })
           })
       })
