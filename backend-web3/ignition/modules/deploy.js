43f1c2717aa716c6dce9a96adb8c4718f5f60589
Samonto Karmaker <samonto0726@gmail.com>
2025-05-08 15:10:23 +0600

    feat: implement contract deployment and verification for NftMart

---
+++ backend-web3/ignition/modules/deploy.js
@@ -0,0 +1,44 @@
+const { ethers, network, run } = require("hardhat")
+const {VERIFICATION_BLOCK_CONFIRMATIONS} = require("../../helper-hardhat-config")
+require("dotenv").config()
+
+const main = async () => {
+    console.log("Deploying contracts...")
+    try {
+        const contractFactory = await ethers.getContractFactory("NftMart")
+        const contract = await contractFactory.deploy()
+        console.log("Contract deployed to address:", contract.target)
+        if (
+            network.config.chainId === 11155111 &&
+            process.env.ETHERSCAN_API_KEY
+        ) {
+            console.log("Waiting for block confirmations...")
+            await contract.deployTransaction.wait(VERIFICATION_BLOCK_CONFIRMATIONS)
+            await verify(contract.target, [])
+        }
+    } catch (error) {
+        console.error("Error deploying contract:", error)
+        process.exit(1)
+    }
+}
+
+const verify = async (contractAddress, args) => {
+    console.log("Verifying contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (err) {
+        if (err.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified")
+        } else {
+            console.log(err)
+        }
+    }
+}
+
+main().catch((err) => {
+    console.error("Error in main function:", err)
+    process.exit(1)
+})
