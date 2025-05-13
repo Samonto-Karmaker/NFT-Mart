00d939f4b3bcaae478b34e745fce35ae0a3f5e1f
Samonto Karmaker <samonto0726@gmail.com>
2025-05-13 15:28:36 +0600

    feat: modify NFT event handlers to manage ActiveItem state

--- subgraph/nft-mart-sepolia/src/nft-mart.ts
+++ subgraph/nft-mart-sepolia/src/nft-mart.ts
@@ -1,53 +1,104 @@
+import { Address, BigInt } from "@graphprotocol/graph-ts"
 import {
-  NftBought as NftBoughtEvent,
-  NftCanceled as NftCanceledEvent,
-  NftListed as NftListedEvent
+    NftBought as NftBoughtEvent,
+    NftCanceled as NftCanceledEvent,
+    NftListed as NftListedEvent,
 } from "../generated/NftMart/NftMart"
-import { NftBought, NftCanceled, NftListed } from "../generated/schema"
+import {
+    NftBought,
+    NftCanceled,
+    NftListed,
+    ActiveItem,
+} from "../generated/schema"
 
 export function handleNftBought(event: NftBoughtEvent): void {
-  let entity = new NftBought(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.buyer = event.params.buyer
-  entity.nftAddress = event.params.nftAddress
-  entity.tokenId = event.params.tokenId
-  entity.price = event.params.price
+    let entity = new NftBought(
+        event.transaction.hash.concatI32(event.logIndex.toI32())
+    )
+    let activeItemId = getIdFromAddressAndTokenId(
+        event.params.nftAddress,
+        event.params.tokenId
+    )
+    let activeItem = ActiveItem.load(activeItemId)
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+    entity.buyer = event.params.buyer
+    entity.nftAddress = event.params.nftAddress
+    entity.tokenId = event.params.tokenId
+    entity.price = event.params.price
 
-  entity.save()
+    activeItem!.buyer = event.params.buyer
+
+    entity.blockNumber = event.block.number
+    entity.blockTimestamp = event.block.timestamp
+    entity.transactionHash = event.transaction.hash
+
+    entity.save()
+    activeItem!.save()
 }
 
 export function handleNftCanceled(event: NftCanceledEvent): void {
-  let entity = new NftCanceled(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.seller = event.params.seller
-  entity.nftAddress = event.params.nftAddress
-  entity.tokenId = event.params.tokenId
+    let entity = new NftCanceled(
+        event.transaction.hash.concatI32(event.logIndex.toI32())
+    )
+    let activeItemId = getIdFromAddressAndTokenId(
+        event.params.nftAddress,
+        event.params.tokenId
+    )
+    let activeItem = ActiveItem.load(activeItemId)
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+    entity.seller = event.params.seller
+    entity.nftAddress = event.params.nftAddress
+    entity.tokenId = event.params.tokenId
 
-  entity.save()
+    activeItem!.buyer = Address.fromString(
+        "0x000000000000000000000000000000000000dEaD" // Cancelled buyer address => 0xdead which is a dead address
+    )
+
+    entity.blockNumber = event.block.number
+    entity.blockTimestamp = event.block.timestamp
+    entity.transactionHash = event.transaction.hash
+
+    entity.save()
 }
 
 export function handleNftListed(event: NftListedEvent): void {
-  let entity = new NftListed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.seller = event.params.seller
-  entity.nftAddress = event.params.nftAddress
-  entity.tokenId = event.params.tokenId
-  entity.price = event.params.price
+    let entity = new NftListed(
+        event.transaction.hash.concatI32(event.logIndex.toI32())
+    )
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+    let activeItemId = getIdFromAddressAndTokenId(
+        event.params.nftAddress,
+        event.params.tokenId
+    )
+    let activeItem = ActiveItem.load(activeItemId)
+    if (!activeItem) {
+        activeItem = new ActiveItem(activeItemId)
+    }
 
-  entity.save()
+    entity.seller = event.params.seller
+    activeItem.seller = event.params.seller
+
+    entity.nftAddress = event.params.nftAddress
+    activeItem.nftAddress = event.params.nftAddress
+
+    entity.tokenId = event.params.tokenId
+    activeItem.tokenId = event.params.tokenId
+
+    entity.price = event.params.price
+    activeItem.price = event.params.price
+
+    activeItem.buyer = Address.fromString(
+        "0x0000000000000000000000000000000000000000" // default buyer address => 0x0 which is a zero address
+    )
+
+    entity.blockNumber = event.block.number
+    entity.blockTimestamp = event.block.timestamp
+    entity.transactionHash = event.transaction.hash
+
+    entity.save()
+    activeItem.save()
+}
+
+function getIdFromAddressAndTokenId(address: Address, tokenId: BigInt): string {
+    return address.toHex() + "-" + tokenId.toString()
 }
